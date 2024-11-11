7ba2037620cf58fbe682bd30abf45c6fc2c93711
DIMI7R0V <danito1718@gmail.com>
2024-11-11 20:51:03 +0200

    11.11

--- MovieStore/MovieStore.BL/Interfaces/IMovieService.cs
+++ MovieStore/MovieStore.BL/Interfaces/IMovieService.cs
@@ -5,5 +5,9 @@ namespace MovieStore.BL.Interfaces
     public interface IMovieService
     {
         List<Movie> GetAllMovies();
+        Movie GetMovieById(int id);
+        void AddMovie(Movie movie);
+        void DeleteMovie(int id);
     }
+
 }
